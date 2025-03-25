3a360d2a587ddb2a3145b0e7e5ec7f3307a63919
BAntDit <bantdit@gmail.com>
2025-03-25 21:00:29 +0300

    fix(TaskWeaver): fixes installation scripts.

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,9 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class TaskWeaverRecipe(ConanFile):
     name = "taskweaver"
-    version = "0.1.0.0"
+    version = "0.3.0.0"
 
-    settings = "build_type"
+    settings = "os", "build_type"
 
     def requirements(self):
         self.requires("gtest/[~1.16]")
@@ -21,3 +21,17 @@ class TaskWeaverRecipe(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "TaskWeaver")
+        self.cpp_info.set_property("cmake_target_name", "TaskWeaver::TaskWeaver")
+
+        self.cpp_info.libs = ["TaskWeaver"]
+        self.cpp_info.requires = ["metrix"]
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["pthread"]
