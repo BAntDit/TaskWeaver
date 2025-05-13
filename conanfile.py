63fd9a146f98ad480fd3353c45b10d9043dd153d
BAntDit <bantdit@gmail.com>
2025-05-13 18:20:50 +0300

    updates build scripts

--- conanfile.py
+++ conanfile.py
@@ -10,21 +10,36 @@ class TaskWeaverRecipe(ConanFile):
     url = "https://github.com/BAntDit/TaskWeaver"
     description = "A multithreaded task scheduler utilizing lock-free task-stealing deques."
 
-    settings = "os", "build_type"
+    settings = "os", "compiler", "arch", "build_type"
 
     exports_sources = "CMakeLists.txt", "*.cmake", ".clang-format", ".md", "src/*.h", "tests/*", "cmake/*"
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.10]")
+        if self.settings.compiler != "msvc":
+            self.tool_requires("ninja/[>=1.11.0]")
 
     def requirements(self):
         self.requires("gtest/[~1.16]")
         self.requires("metrix/[~1.5]")
 
-    def build_requirements(self):
-        self.tool_requires("cmake/[>=3.10]")
+    def configure(self):
+        self.settings.compiler.cppstd = "20"
+
+    def package_id(self):
+        self.info.settings.compiler.cppstd = "20"
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
+
+        if self.settings.compiler == "msvc":
+            tc.generator = "Visual Studio"
+        else:
+            tc.generator = "Ninja"
+
+        tc.variables["REQUIRED_CXX_STANDARD"] = "20"
         tc.generate()
     def build(self):
         cmake = CMake(self)
